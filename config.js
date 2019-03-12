d7aa6863371e6c83fa863dc905898129aa46c8e4
Hugo <hfernandez@neuquen.gov.ar>
2019-03-12 11:12:00 -0300

    Frist commit

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 module.exports = {
 
     sqlConnectionString: "Server=10.1.62.53;Database=sips;User Id=andes;Password=andesIntegration;", // Insert your connection string here.
-    mongoConnectionString: "mongodb://10.1.62.19:27017", // This puts the resulting database in MongoDB running on your local PC.
+    // mongoConnectionString: "mongodb://10.1.62.19:27017", // This puts the resulting database in MongoDB running on your local PC.
+    mongoConnectionString: "mongodb://admin:golitoMon04@10.1.72.7:27033/andes?authSource=admin",
     targetDatabaseName: "andes", // Specify the MongoDB database where the data will end up.
-    targetTable: "PN_smiAfiliados", // Specify the mssql table to export to mongodb.
+    sourceTable: "PN_smiafiliados", // Specify the mssql table to export to mongodb.
+    targetTable: 'sumar',
     targetTableId: "id_smiafiliados",
     skip: [
         "sql-table-to-skip-1", // Add the tables here that you don't want to replicate to MongoDB.
